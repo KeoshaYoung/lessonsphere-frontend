2e40b757abdd7a160345f7ac8f9adbcc397d4ca7
Keosha Young <keoshayoung@Keoshas-iMac.attlocal.net>
2025-04-18 18:37:10 -0400

    Add Render path

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   const handleGenerate = async () => {
     setLoading(true);
-    const response = await fetch("http://127.0.0.1:8000/generate", {
+    const response = await fetch("https://lessonsphere-backend.onrender.com/generate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
